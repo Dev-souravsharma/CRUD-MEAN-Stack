d47e4de88d7fc13ef8ce24ed932c9407bebf39d9
dev-souravsharma <dev.souravsharma@gmail.com>
2021-03-30 12:01:54 +0530

    changed static path name

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(cors());
 
 let api = require('./passwordEncryption/routes/userRoutes');
 // app.use(express.static(__dirname+"dist/Authentication/index.html"));
-app.use(express.static(path.join(__dirname,"dist","CRUD-MEAN")));
-app.use('/',api);
+app.use(express.static(path.join(__dirname,"dist","CRUD-MEAN-Stack")));
+// app.use('/',api);
 app.use((req,res,next)=>{
   // res.setHeader(Access-Control-Allow-Origin ,'*'),
   // res.setHeader(Access-Control-Allow-Methods ,'POST','GET','PUT','DELETE','OPTIONS'),
